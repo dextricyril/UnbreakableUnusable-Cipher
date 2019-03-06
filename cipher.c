7bdd7d824c8aa6c9d55616e9401f3f878716954b
dextricyril <truitecendree@gmail.com>
2019-03-06 18:32:27 +0100

    ADD: Complete cipher decipher, lack key name

--- cipher.c
+++ cipher.c
@@ -13,8 +13,12 @@ int main(int argc, char **argv)
 
     FILE *CB_textFile_FD; //textFile input 
     CB_textFile_FD=fopen(argv[1],"r"); //open file in reading
-
-    FILE *CB_cipherText_FD; // output file
+    if(!CB_textFile_FD) //check if file opened successfully
+    {
+        printf("Failed to open %s", argv[1]);
+        return 1;
+    }
+    FILE *CB_cipherFile_FD; // output file
     FILE *CB_key_FD; // key file
     
     //key file naming
@@ -23,38 +27,47 @@ int main(int argc, char **argv)
 
     if(argc == 3) // user gave output file name
     {
-        CB_cipherText_FD = fopen(argv[2],"w"); // write only output file   
+        CB_cipherFile_FD = fopen(argv[2],"w"); // write only output file   
         //strncpy(CB_keyFileName, argv[2], sizeof(filename)); //TODO
     }
     else
     {
-        CB_cipherText_FD = fopen("ciphered","w"); // write only output file    
+        CB_cipherFile_FD = fopen("ciphered","w"); // write only output file    
     }
 
 
 
     CB_key_FD = fopen("key.pda","w"); // create key File
-    
-    
 
-    if(!CB_textFile_FD)
-    {
-        printf("Failed to open %s", argv[1]);
-        return 1;
-    }
 
 
     char CB_buff_FD, CB_alea_FD , CB_res_FD;
     
+    // get size
+    fseek(CB_textFile_FD, 0, SEEK_END);
+    long CB_len_FD = ftell(CB_textFile_FD);
+    
+    if(CB_len_FD == 0) // check if file is empty
+    {
+         printf("File is empty %ld", CB_len_FD);
+        return 1;
+    }
+
+    fseek(CB_textFile_FD, 0, SEEK_SET); // reset pointer to start of file
     while(!feof(CB_textFile_FD)) // while file can be read
     {
                
         fscanf(CB_textFile_FD, "%c", &CB_buff_FD);
         CB_alea_FD = rand();
         CB_res_FD = CB_buff_FD ^ CB_alea_FD; // bitwise XOR of buff with alea
-        fwrite(&CB_res_FD, sizeof(CB_res_FD), 1, CB_cipherText_FD); //write output file
+        fwrite(&CB_res_FD, sizeof(CB_res_FD), 1, CB_cipherFile_FD); //write output file
         fwrite(&CB_alea_FD, sizeof(CB_alea_FD), 1, CB_key_FD); //write key file
         printf("%c",CB_res_FD); // display output file
     }
+    //close all files
+    fclose(CB_key_FD);
+    fclose(CB_textFile_FD);
+    fclose(CB_cipherFile_FD);
+    return 0;
 
 }
---
+++ decipher.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    if(argc != 3) // 2 arguments 0 is executable name
+    {
+        printf("Did not received correct arguments\nUsage: ./cipher cipheredFile.txt key.pda\n");
+        return 1;
+    }
+
+    FILE *CB_cipheredFile_FD; //cipheredFile input 
+    FILE *CB_keyFile_FD;
+    CB_cipheredFile_FD=fopen(argv[1],"r"); //open file in reading
+    CB_keyFile_FD=fopen(argv[2],"r");
+    if(!CB_cipheredFile_FD || !CB_keyFile_FD) //check if file opened successfully
+    {
+        printf("Failed to open %s or %s", argv[1] , argv[2]);
+        return 1;
+    }
+    char CB_keyBuff_FD, CB_cipherBuff_FD, CB_res_FD;
+    while(!feof(CB_cipheredFile_FD) || !feof(CB_keyFile_FD))
+    {
+        fscanf(CB_cipheredFile_FD,"%c",&CB_cipherBuff_FD); //read char from ciphered file
+        fscanf(CB_keyFile_FD,"%c",&CB_keyBuff_FD); //read char from key file
+        CB_res_FD = CB_cipherBuff_FD ^ CB_keyBuff_FD; // bitwise xor
+        printf("%c",CB_res_FD); // display dicephering results
+    }
+    return 0;
+}
